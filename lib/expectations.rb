acf4c3afab76f3bc426042adbe8dc62d560c6250
Prathamesh Sonpatki <csonpatki@gmail.com>
2013-11-30 01:01:38 +0530

    Added initial code example

--- lib/expectations.rb
+++ lib/expectations.rb
@@ -1,4 +1,27 @@
-require "expectations/version"
+# require "expectations/version"
 
 module Expectations
+  def expect(obj)
+    ExpcetationObject.new(obj)
+  end
 end
+
+class ExpcetationObject
+  def initialize(obj)
+    @object = obj
+  end
+
+  def to_have_key(key)
+    raise ArgumentError unless @object.is_a? Hash
+    yield if @object.keys.include?(key) == false
+  end
+end
+
+
+# Example
+
+# extend Expcetations
+
+# expect(params).to_have_key(:user) do
+#   "Invalid input. Expected key :user to be present"
+# end
